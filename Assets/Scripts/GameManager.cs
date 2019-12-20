105b206dcd580ce36486110b57b7c50d4ba53473
Alexander Isopp <alisopp@edu.aau.at>
2019-12-20 21:34:10 +0100

    added basic game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,15 +7,26 @@ namespace NoMoreLegs
     {
         #region EDITOR_VARIABLES
 
+        [SerializeField] private PlayerController _playerPrefab;
+        [SerializeField] private Transform _startPosition;
+
         #endregion
 
         #region PRIVATE_VARIABLES
 
         private static GameManager _instance;
 
+
+        private PlayerController _currentPlayer;
+
         #endregion
 
         #region UNITY_LIFECYCLE
+
+        public static GameManager GetInstance()
+        {
+            return _instance;
+        }
 
         private void Awake()
         {
@@ -25,6 +36,22 @@ namespace NoMoreLegs
         #endregion
 
         #region METHODS
+
+
+        public void StartGame()
+        {
+            _currentPlayer = Instantiate(_playerPrefab.gameObject).GetComponent<PlayerController>();
+        }
+
+        public void EndGame()
+        {
+            
+        }
+
+        public void PauseGame()
+        {
+            
+        }
 
         #endregion
     }
