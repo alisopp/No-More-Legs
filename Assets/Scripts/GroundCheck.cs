b191d236ba94119fe13d25de472c52e2c2a68c08
Kalidor1 <guidopr@edu.aau.at>
2019-12-20 22:28:38 +0100

    groundCheck

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public start_movement startMovement;
+
+    private int _groundLayer;
     // Start is called before the first frame update
     void Start()
     {
-    
+        _groundLayer = LayerMask.GetMask("Ground");
     }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("ground"))
+        if (collision.gameObject.layer == _groundLayer)
         {
             startMovement.grounded = true;
         }
@@ -22,7 +24,7 @@ public class GroundCheck : MonoBehaviour
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("ground"))
+        if (collision.gameObject.layer == _groundLayer)
             startMovement.grounded = false;
     }
 }
