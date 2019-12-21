eddf2ebe29d302d223207c94b4965010d16726cf
Alexander Isopp <alisopp@edu.aau.at>
2019-12-22 00:44:40 +0100

    added raccoon animations

--- Assets/Scripts/GameState/States/NoMoreLegState.cs
+++ Assets/Scripts/GameState/States/NoMoreLegState.cs
@@ -1,11 +1,17 @@
+using NoMoreLegs.Raccoon;
+using UnityEngine;
+
 namespace NoMoreLegs.GameState
 {
     [System.Serializable]
     public class NoMoreLegState : GameState
     {
+        [SerializeField] private RaccoonController _raccoonController;
         public override void OnStateEnter()
         {
-            GameManager.GetInstance().CurrentPlayer.LoseLegs();
+            GameManager.GetInstance().CurrentPlayer.StopController();
+            _raccoonController.StartStealing();
+            
         }
     }
 }
