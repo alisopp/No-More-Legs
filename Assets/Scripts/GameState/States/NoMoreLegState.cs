e21fb2635297833426bcab28c090765a8a2be511
Kalidor1 <guidopr@edu.aau.at>
2019-12-22 01:20:29 +0100

    Revert "Merge branch 'master' into animation_tilemap"
    This reverts commit 2c9b466362e7ac8e3517d53b420fea117f100d06, reversing
    changes made to 1bf0dc619d5b7c135846405a7879a7d2ec5d3b9a.

--- Assets/Scripts/GameState/States/NoMoreLegState.cs
+++ Assets/Scripts/GameState/States/NoMoreLegState.cs
@@ -1,17 +1,11 @@
-using NoMoreLegs.Raccoon;
-using UnityEngine;
-
 namespace NoMoreLegs.GameState
 {
     [System.Serializable]
     public class NoMoreLegState : GameState
     {
-        [SerializeField] private RaccoonController _raccoonController;
         public override void OnStateEnter()
         {
-            GameManager.GetInstance().CurrentPlayer.StopController();
-            _raccoonController.StartStealing();
-            
+            GameManager.GetInstance().CurrentPlayer.LoseLegs();
         }
     }
 }
